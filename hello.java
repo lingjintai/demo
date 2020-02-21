c011f10ba765d73e988722ad1b7a4815f4b461ee
lingjintai <13156699719@163.com>
2020-02-21 20:46:28 +0800

    hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+  System.out.println("Hello World11");
+  System.out.println("Hello World22");
+  System.out.println("Hello World33");
+    }
+}
